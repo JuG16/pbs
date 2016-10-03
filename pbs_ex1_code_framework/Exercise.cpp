70a7a3de29f0a2e6bcabffb75a92735bb71f2e08
fasin95 <huberya@student.ethz.ch>
2016-10-03 18:31:31 +0200

    added task 3

--- pbs_ex1_code_framework/Exercise.cpp
+++ pbs_ex1_code_framework/Exercise.cpp
@@ -4,6 +4,7 @@
 //=============================================================================
 
 #include "Utilities/Vector2T.h"
+#include <algorithm>
 
 // Gravitational acceleration (9.81 m/s^2)
 static const double g = 9.81;
@@ -45,7 +46,7 @@ void AdvanceTimeStep1(double k, double m, double d, double L, double dt, int met
 		//v2 = v2 + dt*(((-k*(p2 - L) + (m*g)) - d*v2) / m);
 		double alpha = -d / 2 * m;
 		double beta = std::sqrt(4 * k*m - d*d) / (2 * m);
-		p2 = m*g / k*std::exp(alpha*dt)*std::cos(beta*dt) + d*g / (2 * k)*std::exp(alpha*dt)*std::sin(beta*dt) - L - m*g / k;
+		p2 = m*g / k*std::exp(alpha*dt)*std::cos(beta*dt) + d*g / (2 * k)*std::exp(alpha*dt)*std::sin(beta*dt) - L - m*g / k; //- or + L?
 	}
 
 	default:
@@ -58,5 +59,12 @@ void AdvanceTimeStep1(double k, double m, double d, double L, double dt, int met
 void AdvanceTimeStep3(double k, double m, double d, double L, double dt,
                       Vec2& p1, Vec2& v1, Vec2& p2, Vec2& v2, Vec2& p3, Vec2& v3)
 {
-	p1 += Vec2(1,1);
+	const Vec2 gvec = Vec2(0, -9.81);
+	const Vec2 kresp = Vec2(0, 100);
+	v1 = v1 + dt*((1. / 2 * k*(((p1 - p2).norm() - L)*(p2 - p1).normalized() + ((p1 - p3).norm() - L)*(p3 - p1).normalized()) + m*gvec - d*v1+kresp*std::max(0.,-1-p1.y())) / m);
+	v2 = v2 + dt*((1. / 2 * k*(((p2 - p1).norm() - L)*(p1 - p2).normalized() + ((p2 - p3).norm() - L)*(p3 - p2).normalized()) + m*gvec - d*v2+kresp*std::max(0., -1 - p2.y())) / m);
+	v3 = v3 + dt*((1. / 2 * k*(((p3 - p1).norm() - L)*(p1 - p3).normalized() + ((p3 - p2).norm() - L)*(p2 - p3).normalized()) + m*gvec - d*v3+kresp*std::max(0., -1 - p3.y())) / m);
+	p1 = p1 + dt*v1;
+	p2 = p2 + dt*v2;
+	p3 = p3 + dt*v3;
 }
