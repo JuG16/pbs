9da62686be5bcc8855d49662d9ebb95aaf526b7f
fasin95 <huberya@student.ethz.ch>
2016-10-15 20:55:18 +0200

    task 1

--- pbs_ex2_code_framework/FEMElementTri.cpp
+++ pbs_ex2_code_framework/FEMElementTri.cpp
@@ -24,5 +24,26 @@ void FEMElementTri::computeSingleBasisDerivGlobalGeom(int nodeId, Vector2 &basis
 // TASK 1
 void FEMElementTri::computeSingleBasisDerivGlobalLES(int nodeId, Vector2 &basisDerivGlobal, const FEMMesh *pMesh) const
 {
-	
+	Matrix3x3 paramMatrix;
+
+	paramMatrix(0, 0) = (*pMesh).GetNodePosition(this->GetGlobalNodeForElementNode(0)).x();
+	paramMatrix(0, 1) = (*pMesh).GetNodePosition(this->GetGlobalNodeForElementNode(0)).y();
+	paramMatrix(0, 2) = 1;
+	paramMatrix(1, 0) = (*pMesh).GetNodePosition(this->GetGlobalNodeForElementNode(1)).x();
+	paramMatrix(1, 1) = (*pMesh).GetNodePosition(this->GetGlobalNodeForElementNode(1)).y();
+	paramMatrix(1, 2) = 1;
+	paramMatrix(2, 0) = (*pMesh).GetNodePosition(this->GetGlobalNodeForElementNode(2)).x();
+	paramMatrix(2, 1) = (*pMesh).GetNodePosition(this->GetGlobalNodeForElementNode(2)).y();
+	paramMatrix(2, 2) = 1;
+
+	Vector3 rhs;
+	rhs.setX(this->GetGlobalNodeForElementNode(0) == nodeId ? 1 : 0);
+	rhs.setY(this->GetGlobalNodeForElementNode(1) == nodeId ? 1 : 0);
+	rhs.setZ(this->GetGlobalNodeForElementNode(2) == nodeId ? 1 : 0);
+
+
+	Vector3 params = paramMatrix.inverse()*rhs;
+
+	basisDerivGlobal.x() = params.x();
+	basisDerivGlobal.y() = params.y();
 }
