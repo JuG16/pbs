e49fa58c0de2fc317ace7889c011f062fcddf80b
fasin95 <huberya@student.ethz.ch>
2016-10-26 21:05:28 +0200

    task1 (without convergence interrupt) how to compute residual?

--- pbs_ex3_code_framework/exercise.cpp
+++ pbs_ex3_code_framework/exercise.cpp
@@ -12,20 +12,26 @@ void ExSolvePoisson(int _xRes, int _yRes, int _iterations, double _accuracy, Arr
 {
 	double dx = 1.0 / _xRes;
 	
-	// Note that the boundaries are handles by the framework, so you iterations should be similar to:
-	for (int y = 1; y < _yRes - 1; y++)
+	for (int it = 0; it < _iterations; ++it)
 	{
+		// Note that the boundaries are handles by the framework, so you iterations should be similar to:
+		//switched around order for caching purposes (correct?)
 		for (int x = 1; x < _xRes - 1; x++)
 		{
-			
+			for (int y = 1; y < _xRes - 1; y++)
+			{
+				_field(x, y) = 1.*(_b(x, y) + _field(x - 1, y) + _field(x, y - 1) + _field(x + 1, y) + _field(x, y + 1)) / 4;
+			}
 		}
+		// For your debugging, and ours, please add these prints after every iteration
+		//how to compute the residual???
+		/*if(it == _iterations - 1) 
+			printf("Pressure solver: it=%d , res=%f \n", it, residual);
+		if(residual < _accuracy)
+			printf("Pressure solver: it=%d , res=%f, converged \n", it, residual);*/
 	}
 	
-	// For your debugging, and ours, please add these prints after every iteration
-	//if(it == _iterations - 1) 
-	//	printf("Pressure solver: it=%d , res=%f \n", it, residual);
-	//if(residual < _accuracy)
-	//	printf("Pressure solver: it=%d , res=%f, converged \n", it, residual);
+	
 }
 
 // Problem 2
@@ -33,6 +39,7 @@ void ExCorrectVelocities(int _xRes, int _yRes, double _dt, const Array2d &_press
 {
 	double dx = 1.0 / _xRes;
 
+	
 	// Note: velocity u_{i+1/2} is practically stored at i+1
 }
 
