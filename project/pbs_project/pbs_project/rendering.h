e1af47ae88b914bd279d075b7b5db0cce8f07ff9
jgiger <jgiger@student.ethz.ch>
2016-11-22 16:54:01 +0100

    ogre3d

--- project/pbs_project/pbs_project/rendering.h
+++ project/pbs_project/pbs_project/rendering.h
@@ -3,6 +3,10 @@
 #include "../include/ogre/build/sdk/include/OGRE/Ogre.h" //instead of last part put <"sometext"> from API
 #include "sphere.h"
 #include "typedef.h"
+#include "../include/ogre/OgreMain/include/OgreSceneManager.h"
+#include "../include/ogre/OgreMain/include/OgreRoot.h"
+
+
 
 
 //template <typename container>
@@ -12,7 +16,7 @@ void drawframe()
 	//do plotting using ogre3d
 	//sphere is a container with random access "[]"
 	//every element is of type "sphere"
-	Ogre::SceneManager* mSceneMgr=SceneManager();
+	Ogre::SceneManager* mSceneMgr= createSceneManager(Ogre::ST_GENERIC);
 	mSceneMgr->setAmbientLight(Ogre::ColourValue(0.5, 0.5, 0.5));
 	Ogre::Entity* ogreEntity = mSceneMgr->createEntity("ogrehead.mesh");
 	Ogre::SceneNode* ogreNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
