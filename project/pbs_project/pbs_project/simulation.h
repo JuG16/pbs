94abed0a9bdf33ca57a2ac54adb00db4f3fa62f3
fasin95 <huberya@student.ethz.ch>
2016-11-22 16:24:48 +0100

    some initialization

--- project/pbs_project/pbs_project/simulation.h
+++ project/pbs_project/pbs_project/simulation.h
@@ -3,33 +3,86 @@
 #include "sphere.h"
 #include "vehicle.h"
 
-template <typename container>
-void init(container &spheres, vehicle &car)
-{
-	//initialize system -> set initial positions and velocities of all components
-	//sphere is a container with random access "[]"
-	//every element is of type "sphere"
-}
 
-template <typename container>
-void step(container &spheres, const int_t n_spheres, vehicle &car)
+class simulation
 {
-	//sphere is a container with random access "[]"
-	//every element is of type "sphere"
-	vector_t forces;
-	forces.resize(3 * n_spheres);
-	
-	//compute contact points (really stupid for now, use better datastructure later)
-	for (int i = 0; i < n_spheres; ++i)
+public:
+	template <typename container>
+	simulation(container &spheres, int_t n_spheres, vehicle &car):n_spheres_(n_spheres)
 	{
-		for (int j = 0; j < n_spheres; ++j)
+		//initialize system -> set initial positions and velocities of all components
+		//sphere is a container with random access "[]"
+		//every element is of type "sphere"
+		forces_.resize(6 * (n_spheres+1)); //+1 in size is for car
+		velocities_.resize(6 * (n_spheres+1));
+		massmatrix_.resize(6 * (n_spheres+1), 6 * (n_spheres+1);
+		massmatrix_.reserve(12 * (n_spheres + 1));
+		std::vector<Eigen::Triplet<real_t>> triplets;
+		triplets.reserve(12 * (n_spheres + 1));
+		for(int i=0;i<n_spheres;++i)
 		{
-			if ((spheres[i].getpos() - spheres[j].getpos()).norm() < spheres[i].getrad() + spheres[j].getrad())
+			triplets.push_back(Eigen::Triplet(6 * i, 6 * i, sphere[i].getmass()));
+			triplets.push_back(Eigen::Triplet(6 * i + 1, 6 * i + 1, sphere[i].getmass()));
+			triplets.push_back(Eigen::Triplet(6 * i + 2, 6 * i + 2, sphere[i].getmass()));
+
+			mat3d tempinertia = spheres[i].getinertia();
+			triplets.push_back(Eigen::Triplet(6 * i + 3, 6 * i + 3, tempinertia(0, 0)));
+			triplets.push_back(Eigen::Triplet(6 * i + 3, 6 * i + 4, tempinertia(0, 1)));
+			triplets.push_back(Eigen::Triplet(6 * i + 3, 6 * i + 5, tempinertia(0, 2)));
+			triplets.push_back(Eigen::Triplet(6 * i + 4, 6 * i + 3, tempinertia(1, 0)));
+			triplets.push_back(Eigen::Triplet(6 * i + 4, 6 * i + 4, tempinertia(1, 1)));
+			triplets.push_back(Eigen::Triplet(6 * i + 4, 6 * i + 5, tempinertia(1, 2)));
+			triplets.push_back(Eigen::Triplet(6 * i + 5, 6 * i + 3, tempinertia(2, 0)));
+			triplets.push_back(Eigen::Triplet(6 * i + 5, 6 * i + 4, tempinertia(2, 1)));
+			triplets.push_back(Eigen::Triplet(6 * i + 5, 6 * i + 5, tempinertia(2, 2)));
+		}
+		triplets.push_back(Eigen::Triplet(6 * i, 6 * i, car.getmass()));
+		triplets.push_back(Eigen::Triplet(6 * i + 1, 6 * i + 1, car.getmass()));
+		triplets.push_back(Eigen::Triplet(6 * i + 2, 6 * i + 2, car.getmass()));
+
+		mat3d tempinertia = car.getinertia();
+		triplets.push_back(Eigen::Triplet(6 * i + 3, 6 * i + 3, tempinertia(0, 0)));
+		triplets.push_back(Eigen::Triplet(6 * i + 3, 6 * i + 4, tempinertia(0, 1)));
+		triplets.push_back(Eigen::Triplet(6 * i + 3, 6 * i + 5, tempinertia(0, 2)));
+		triplets.push_back(Eigen::Triplet(6 * i + 4, 6 * i + 3, tempinertia(1, 0)));
+		triplets.push_back(Eigen::Triplet(6 * i + 4, 6 * i + 4, tempinertia(1, 1)));
+		triplets.push_back(Eigen::Triplet(6 * i + 4, 6 * i + 5, tempinertia(1, 2)));
+		triplets.push_back(Eigen::Triplet(6 * i + 5, 6 * i + 3, tempinertia(2, 0)));
+		triplets.push_back(Eigen::Triplet(6 * i + 5, 6 * i + 4, tempinertia(2, 1)));
+		triplets.push_back(Eigen::Triplet(6 * i + 5, 6 * i + 5, tempinertia(2, 2)));
+
+		massmatrix_.setFromTriplets(triplets.begin(), triplets.end());
+		massmatrix_.makeCompressed();
+	}
+
+	template <typename container>
+	void step(container &spheres, vehicle &car)
+	{
+		//sphere is a container with random access "[]"
+		//every element is of type "sphere"
+		for (int i = 0; i < n_spheres; ++i)
+		{
+			velocities(6 * i, 6 * i + 2) = spheres[i].getvel();
+			velocities(6 * i + 3, 6 * i + 5) = spheres[i].getangvel();
+		}
+
+		//compute contact points (really stupid for now, use better datastructure later)
+		for (int i = 0; i < n_spheres; ++i)
+		{
+			for (int j = 0; j < n_spheres; ++j)
 			{
-				//contact
+				if ((spheres[i].getpos() - spheres[j].getpos()).norm() < spheres[i].getrad() + spheres[j].getrad())
+				{
+					//contact
+				}
 			}
 		}
+		//compute forces and store them in forces vector
 	}
-	//compute forces and store them in forces vector
+private:
+	int_t n_spheres_;
+	vector_t forces_; //is denoted as F_c in paper "Iterative Dynamics"
+	vector_t velocities_; //is denoted as V in paper "Iterative Dynamics"
+	smatrix_t massmatrix_; //is denoted a M in paper "iterative Dynamics"
+};
 
-}
--- project/pbs_project/pbs_project/typedef.h
+++ project/pbs_project/pbs_project/typedef.h
@@ -1,9 +1,12 @@
 #pragma once
 
 #include "../include/eigen3/Eigen/Dense"
+#include "../include/eigen3/Eigen/Sparse"
 
 typedef double real_t;
 typedef int int_t;
 typedef Eigen::Matrix<real_t, Eigen::Dynamic, Eigen::Dynamic> matrix_t; //matrix with dynamic size
+typedef Eigen::SparseMatrix<real_t> smatrix_t;
 typedef Eigen::Matrix<real_t, Eigen::Dynamic, 1> vector_t; //col vector or as vector?
+typedef Eigen::Matrix<real_t, 3, 3> mat3d; //3x3 matrix
 typedef Eigen::Matrix<real_t, 3, 1> vec3d; //3 dimensional column vector
