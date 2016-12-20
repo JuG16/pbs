326256bee989645204366766d9b747d3c0e6a8d7
fasin95 <huberya@student.ethz.ch>
2016-12-20 16:31:24 +0100

    still terrible

--- project/pbs_project/pbs_project/main.cpp
+++ project/pbs_project/pbs_project/main.cpp
@@ -113,7 +113,7 @@ int main(int argc, char** argv)
 	float box_thickness = 2;
 	float box_offset_y = (plane_thickness / 2) + (box_height / 2);
 	float box_length_outside_x = box_size_inside_x+(2 * box_thickness);
-	objects.push_back(new box(vec3d(box_offset_x, box_offset_y, box_offset_z), 1000 * Eigen::MatrixXd::Identity(3, 3), 9000, box_thickness, box_height, box_size_inside_z, vec3d(0, 0, 0)));
+	/*objects.push_back(new box(vec3d(box_offset_x, box_offset_y, box_offset_z), 1000 * Eigen::MatrixXd::Identity(3, 3), 9000, box_thickness, box_height, box_size_inside_z, vec3d(0, 0, 0)));
 	objects[o]->setstatic();
 	o++;
 	objects.push_back(new box(vec3d(box_offset_x+box_thickness+box_size_inside_x, box_offset_y, box_offset_z), 1000 * Eigen::MatrixXd::Identity(3, 3), 9000, box_thickness, box_height, box_size_inside_z, vec3d(0, 0, 0)));
@@ -124,7 +124,7 @@ int main(int argc, char** argv)
 	o++;
 	objects.push_back(new box(vec3d(box_offset_x + ((box_thickness + box_size_inside_x) / 2), box_offset_y, box_offset_z + ((box_size_inside_z + box_thickness) / 2)), 1000 * Eigen::MatrixXd::Identity(3, 3), 9000, box_length_outside_x, box_height, box_thickness, vec3d(0, 0, 0)));
 	objects[o]->setstatic();
-	o++;
+	o++;*/
 
 	//slide
 	objects.push_back(new box(vec3d(-30, 90, box_offset_z), 1000 * Eigen::MatrixXd::Identity(3, 3), 9000, 1, 100, 20, vec3d(0, 0, 0), vec3d(0, 0, 0), quat));
@@ -136,18 +136,18 @@ int main(int argc, char** argv)
 	//o++;
 
 	float start_x = 10; //55
-	float start_y = 10;//6
+	float start_y = 6;//6
 	float start_z = -15; //-20
-	const int x_grid = 9;
-	const int y_grid = 3;
-	const int z_grid = 4;
-	const real_t diameter = 2 * radius_sphere;
+	const int x_grid = 5;
+	const int y_grid = 4;
+	const int z_grid = 5;
+	const real_t diameter = 2 * radius_sphere+0.001;
 	for (int i = 0; i < x_grid; i++){
 		for (int j = 0; j < y_grid; j++) {
 			for (int k = 0; k < z_grid; k++) {
 				objects.push_back(new sphere(vec3d((i*diameter)+start_x, (j*diameter) + start_y,(k*diameter)+start_z)));
 				//objects[o]->setstatic();
-				//o++;
+				o++;
 			}	
 		}
 	}
--- project/pbs_project/pbs_project/simulation.h
+++ project/pbs_project/pbs_project/simulation.h
@@ -8,6 +8,7 @@
 #include "gjk_algorithm.h"
 #include <iostream>
 #include <iomanip>
+#include <cassert>
 
 
 //#define DEBUG
@@ -429,27 +430,52 @@ public:
 		//probably have to use some iterative solvers instead ?
 		//compute eta
 		eta_ = 1./dt*coll_resolve_-jacobian_*(1. / dt*velocities_ + massmatrixinv_*fext_);
-		//can we assume J*M^-1*J^T is sparse?
 		//compute lambda
-		//do 3 steps to utilize sparsity (doesnt work since parts are not squared)
-		ssolver solver;
-		//solve for M^-1 J^T lambda
-		/*solver.analyzePattern(jacobian_);
-		solver.factorize(jacobian_);
-		auto MJlambda = solver.solve(eta_);
-		//solve for J^T lambda
-		solver.analyzePattern(massmatrixinv_);
-		solver.factorize(massmatrixinv_);
-		auto Jlambda = solver.solve(MJlambda);
-		//solve for lambda
-		solver.analyzePattern(jacobian_.transpose());
-		solver.factorize(jacobian_.transpose());
-		auto lambda = solver.solve(Jlambda);*/
+		/*matrix_t A = jacobian_*massmatrixinv_*jacobian_.transpose();
+		const int maxiter = 1000;
+		const real_t relaxation = 1.;
+		const real_t tolerance = 0.00000000001;
+		real_t delta;
+		vector_t lambda = lambda_cache_;
+		for (int iter = 0; iter < maxiter; ++iter)
+		{
+			real_t error = 0.;
+			for (int i = 0; i < eta_.rows(); ++i)
+			{
+				delta = 0.;
+				for (int j = 0; j < i; ++j)
+				{
+					delta += A(i, j)*lambda(j);
+				}
+				for (int j = i + 1; j < eta_.rows(); ++j)
+				{
+					delta += A(i, j)*lambda(j);
+				}
 
-		//solver.analyzePattern(jacobian_*massmatrixinv_*jacobian_.transpose());
-		//solver.factorize(jacobian_*massmatrixinv_*jacobian_.transpose());
+				delta = (eta_(i) - delta) / A(i, i);
+				error = std::max(error, std::abs(delta - lambda(i)));
+				lambda(i) += relaxation*(delta - lambda(i));
+			}
+			if (error < tolerance)
+			{
+				break;
+			}
+			
+		}*/
+
+
+		ssolver solver;
+	
+
 		solver.compute(jacobian_*massmatrixinv_*jacobian_.transpose()); //problem: if all zero can get any result
-		vector_t lambda = solver.solve(eta_);
+		vector_t lambda = solver.solveWithGuess(eta_, lambda_cache_);
+		if (solver.error() > 1)
+		{
+			std::cout << solver.error() << std::endl;
+		}
+
+		//matrix_t A = jacobian_*massmatrixinv_*jacobian_.transpose();
+		//vector_t lambda = A.colPivHouseholderQr().solve(eta_);
 		for (int ij = 0; ij < lambda.size(); ++ij)
 		{
 			if (lambda_bool_(ij/3))
@@ -479,11 +505,7 @@ public:
 
 #endif // DEBUG
 		
-		//update velocities schwachsinn??
-		//solver.analyzePattern(massmatrix_);
-		//solver.factorize(massmatrix_);
-		//velocities_=solver.solve(dt*(jacobian_.transpose()*lambda+fext_)+massmatrix_*velocities_)
-		
+			
 		//correct?
 #ifdef DEBUG
 
@@ -504,7 +526,6 @@ public:
 		std::cout << "check4" << std::endl;
 
 #endif // DEBUG
-		//set velocity of car
 		for (int i = 0; i < n_objects_; ++i)
 		{
 			//position update
